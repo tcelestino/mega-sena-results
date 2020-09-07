963ffd50ed2c3fc073c9384a7dfe08a524632ac3
Tiago Celestino <tcelestino@gmail.com>
2020-09-07 19:44:43 -0300

    chore: remove airbnb config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: ['airbnb', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   plugins: ['@typescript-eslint'],
   parserOptions: {
     project: './tsconfig.json',
