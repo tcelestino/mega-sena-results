c8afd64d85c77c7b4a096623cd07ce018ae38a5c
Tiago Celestino <tcelestino@gmail.com>
2020-09-23 21:04:59 -0300

    create environments variables

---
+++ src/envs.js
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+require('dotenv').config();
+
+module.exports = {
+  EMAIL_SERVICE: process.env.EMAIL_SERVICE,
+  EMAIL_HOST: process.env.EMAIL_HOST,
+  EMAIL_PORT: process.env.EMAIL_PORT,
+  EMAIL_USER: process.env.EMAIL_USER,
+  EMAIL_PASS: process.env.EMAIL_PASS,
+  PAGE_URL: process.env.PAGE_URL,
+};
